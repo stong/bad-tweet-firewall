67dffb10dde8cf2cb16b3a9e9c832031d9228f0f
stong <stong@users.noreply.github.com>
2024-01-23 05:12:54 -0500

    shit for twitter

--- Source/content_script.js
+++ Source/content_script.js
@@ -1,12 +1,26 @@
-document.addEventListener("DOMNodeInserted", function(e) {
-  walk(document.body);
-}, false);
+if (window.MutationObserver) {
+	var observer = new MutationObserver(function (mutations) {
+		Array.prototype.forEach.call(mutations, function (m) {
+			if (m.type === 'childList') {
+				walk(m.target);
+			} else if (m.target.nodeType === 3) {
+				handleText(m.target);
+			}
+		});
+	});
+
+	observer.observe(document.body, {
+		childList: true,
+		attributes: false,
+		characterData: true,
+		subtree: true
+	});
+}
+
+walk(document.body);
 
 function walk(node) 
 {
-	// I stole this function from here:
-	// http://is.gd/mwZp7E
-	
 	var child, next;
 	
 	var tagName = node.tagName ? node.tagName.toLowerCase() : "";
@@ -15,6 +29,21 @@ function walk(node)
 	}
 	if (node.classList && node.classList.contains('ace_editor')) {
 		return;
+	}
+	// special bullshit for twitter. this really should be a more general algorithm but its 5 am lol
+	if (tagName == 'span') {
+		next = node.nextSibling;
+		if (next && next.tagName && next.tagName.toLowerCase() == tagName) {
+			var next2 = next.nextSibling;
+			if (next2 && next2.tagName && next2.tagName.toLowerCase() == tagName) {
+				if (next.textContent === " ") {
+					var result = handleSpecial(node, next2);
+					if (result) {
+						return;
+					}
+				}
+			}
+		}
 	}
 
 	switch ( node.nodeType )  
@@ -37,16 +66,36 @@ function walk(node)
 	}
 }
 
+function handleSpecial(node1, node2)
+{
+	var tc1 = node1.textContent;
+	var tc2 = node2.textContent;
+	if (tc1.toLowerCase() === "machine" && tc2.toLowerCase() === "learning") {
+		tc1 = tc1.replace(/\bMachine\b/g, "Money");
+		tc1 = tc1.replace(/\bmachine\b/g, "money");
+		tc2 = tc2.replace(/\bLearning\b/g, "Laundering");
+		tc2 = tc2.replace(/\blearning\b/g, "laundering");
+		node1.textContent = tc1;
+		node2.textContent = tc2;
+		return true;
+	}
+	return false;
+}
+
 function handleText(textNode) 
 {
-	var v = textNode.nodeValue;
+	var oldValue = textNode.nodeValue;
+	var v = oldValue;
 
 	v = v.replace(/\bMachine Learning\b/g, "Money Laundering");
 	v = v.replace(/\bMachine learning\b/g, "Money laundering");
 	v = v.replace(/\bmachine Learning\b/g, "money Laundering");
 	v = v.replace(/\bmachine learning\b/g, "money laundering");
 	
-	textNode.nodeValue = v;
+	// avoid infinite series of DOM changes
+	if (v !== oldValue) {
+		textNode.nodeValue = v;
+	}
 }
 
 
